d27fcb17c8b95ab113c1a843dd6fe8ab58de7fb2
Assan-Bakhyt <asanbahit@gmail.com>
2025-02-14 21:43:25 +0500

    LAB3

--- LAB3/Functions1/14.py
+++ LAB3/Functions1/14.py
@@ -1,26 +1,25 @@
 import random 
 
-def myfunction(): 
-    random_number = random.randint(1, 20)
+random_number = random.randint(1, 20)
 
-    c = 0
+c = 0
 
-    print("Hello! What is your name?")
-    a = input("Name: ")
+print("Hello! What is your name?")
+a = input("Name: ")
 
-    print(f"Well, {a}, I am thinking of a number between 1 and 20.\nTake a guess.")
+print(f"Well, {a}, I am thinking of a number between 1 and 20.\nTake a guess.")
+b = int(input("Enter: "))
+
+while(True):
+    c += 1
+
+    if b < random_number:
+        print("Your guess is too low.\nTake a guess.")
+    elif b > random_number:
+        print("Your guess is too high.\nTake a guess.")
+    else:
+        print(f"Good job, {a}! You guessed my number in {c} guesses!")
+        break 
+
+  
     b = int(input("Enter: "))
-
-    while(True):
-        c += 1
-
-        if b < random_number:
-            print("Your guess is too low.\nTake a guess.")
-        elif b > random_number:
-            print("Your guess is too high.\nTake a guess.")
-        else:
-            print(f"Good job, {a}! You guessed my number in {c} guesses!")
-            break 
-
-    
-        b = int(input("Enter: "))
